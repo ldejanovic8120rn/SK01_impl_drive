3cafacdcf413e8516703aea23b32b6babbde8fe2
Lazar Dejanovic <ldejanovic8120rn@raf.rs>
2021-11-05 20:38:19 +0100

    GooleDriveImpl - GMTA

--- src/main/java/com/driveimpl/DriveFileChecker.java
+++ src/main/java/com/driveimpl/DriveFileChecker.java
@@ -1,17 +1,56 @@
 package com.driveimpl;
 
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 import com.storage.FileChecker;
+import com.utils.StorageInfo;
+
+import java.io.IOException;
 
 public class DriveFileChecker extends FileChecker {
 
     @Override
     public boolean ckeckPath(String path) {
-        return false;
+        java.io.File file = new java.io.File(path);
+        return file.exists();
     }
 
+    // Storage/A/B/C/fajl.txt
     @Override
     public boolean ckeckStoragePath(String path) {
-        return false;
+        String[] files = path.split("/");
+        File root = GoogleDrive.getFile(files[0]);
+
+        for (int i = 1; i < files.length; i++) {
+            String query = "name='" + files[i] + "'";
+            FileList list = null;
+            try {
+                list = GoogleDrive.service.files().list().setQ(query).setFields("nextPageToken, files(id, name, size, createdTime, mimeType, modifiedTime, parents)").execute();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (list == null) {
+                return false;
+            }
+
+            File child = null;
+            for (File file: list.getFiles()) {
+                if (file.getParents().get(0).equals(root.getId())) {
+                    child = file;
+                    break;
+                }
+            }
+
+            if (child == null) {
+                return false;
+            }
+
+            root = child;
+        }
+
+        return true;
     }
 
     @Override
